efbb49a2e3436c95616d80e01ad251667ee964be
Dmitry <haseprogram@mail.ru>
2018-02-17 22:06:56 +0300

    Added server

---
+++ server/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+ const express = require('express');
+ const app = express();
+
+app.use(express.static('public'));
+
+ const port = process.env.PORT || 8080;
+ app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+ });
