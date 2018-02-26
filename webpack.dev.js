8d7f08028ddd4ee05fe14b5cda7609cacfa07616
Dmitry <haseprogram@mail.ru>
2018-02-26 23:47:35 +0300

    [FIX] webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/app.js',
+	entry: './src/main.js',
 	output: {
 		path: path.resolve(__dirname, 'public'),
 		filename: 'bundle.js'
