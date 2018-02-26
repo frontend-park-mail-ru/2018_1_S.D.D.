44a82696ffd732d1dc6e5eb21192eb6bfbcf57aa
Dmitry <haseprogram@mail.ru>
2018-02-26 23:14:39 +0300

    [FIX] eslint, webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,17 @@
-const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, "public"),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader'
-            }
-        ]
-    }
-}
+	entry: './src/app.js',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.pug$/,
+				loader: 'pug-loader'
+			}
+		]
+	}
+};
