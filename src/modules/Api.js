56c14b1198cb1c6dc941e258220d07f78f4d500c
Dmitry <haseprogram@mail.ru>
2018-03-07 15:17:54 +0300

    Started with API

---
+++ src/modules/Api.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ *  Sends requests to server. 
+ */ 
+class Api {
+	/**
+	 * Creates Api instance
+	 * 
+	 * @param {string} serverUrl Server address.
+	 */
+	constructor(serverUrl) {
+		this.serverUrl = serverUrl;
+	}
+
+	
+}
+
+export default Api;
