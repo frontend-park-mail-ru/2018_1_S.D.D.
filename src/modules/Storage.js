f26a091a4061da8541d629982445ce0db40e4f2f
Dmitry <haseprogram@mail.ru>
2018-03-30 17:53:26 +0300

    LocalStorage implementation

--- src/modules/Storage.js
+++ src/modules/Storage.js
@@ -1,6 +1,11 @@
 'use strict';
 
 /**
+ * Will replace after event-bus implementation would be added.
+ */
+class EventBus {};
+
+/**
  * Creates instance of Storage
  * 
  * @class
@@ -16,19 +21,64 @@ class Storage {
 		}
 		Storage._instance = this;
 
-		this._storage = [];
-
+		this._storage = new EventBus();
 		window.addEventListener('storage', e => {
-			if(this._storage[e.key]) {
-				this._storage[e.key].forEach(callback => {
-					callback(this.grab(e.key), e);
-				});
-			}
+			this._storage.emit(`Storage:${e.key}`, e);
 		});
 	}
 
-	grab(key) {
+	/**
+	 * Subscribe on event when data in localstorage changed.
+	 * 
+	 * @param {string} key Data changed in storage. 
+	 * @param {Function} callback Function to evaluate when data changed.
+	 */
+	onChange(key, callback) {
+		this._storage.subscribe(`Storage:${key}`, callback);
+	}
+
+	/**
+	 * Remove onChange listener on data.
+	 * 
+	 * @param {string} key Data changed in storage.
+	 */
+	removeChangeLisener(key) {
+		this._storage.unSubscribe(key);
+	}
+
+	/**
+	 * Get data from localStorage.
+	 * 
+	 * @param {string} key Data in storage
+	 */
+	getData(key) {
 		return localStorage.getItem(key);
+	}
+
+	/**
+	 * Insert (update) data in localStorage.
+	 * 
+	 * @param {string} key Data name in localStorage.
+	 * @param {*} value Data value in localStorage.
+	 */
+	setData(key, value) {
+		localStorage.setItem(key, value);
+	}
+
+	/**
+	 * Remove data from localStorage.
+	 * 
+	 * @param {string} key Data name in localStorage.
+	 */
+	removeData(key) {
+		localStorage.removeItem(key);
+	}
+
+	/**
+	 * Clear localStorage.
+	 */
+	clear() {
+		localStorage.clear();
 	}
 
 
