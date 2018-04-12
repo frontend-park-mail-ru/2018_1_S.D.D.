1e7407b19bfe17e7ad36c4272f70f69822f29bd0
HustonMavr <huston.mavr@gmail.com>
2018-04-13 02:31:53 +0300

    refactored random

--- src/modules/game/Bot.ts
+++ src/modules/game/Bot.ts
@@ -1,5 +1,6 @@
 'use strict';
 import Player from './Player';
+import { Direction } from './Direction';
 
 export default class Bot extends Player {
     constructor (id:number) {
@@ -7,7 +8,12 @@ export default class Bot extends Player {
     }
 
     directionSwtich () {
-        this._direction = Math.floor(Math.random() * (5 - 1)) + 1;
+        const directionKeys = Object.keys(Direction).filter(k => typeof Direction[k as any] === "number");
+        const directionValues = directionKeys.map(k => parseInt(Direction[k as any]));
+        const max = Math.max.apply(null, directionValues);
+        const min = Math.min.apply(null, directionValues);
+
+        this._direction = Math.floor(Math.random() * max) + min;
         console.log(this._id,this._direction);
     }
 }
