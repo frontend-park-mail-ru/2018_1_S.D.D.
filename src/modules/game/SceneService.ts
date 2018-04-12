dd93c6b3fe2b7bedeafd738fd35fd6eebc35b464
HustonMavr <huston.mavr@gmail.com>
2018-04-12 20:18:12 +0300

    i'm not govnorukij

--- src/modules/game/SceneService.ts
+++ src/modules/game/SceneService.ts
@@ -1,16 +1,19 @@
 'use strict';
 import GameField from "./GameField";
 import Player from "./Player";
-import {FIELD_SIZE, PLAYER_MAP} from "./defines";
+import {FIELD_SIZE, PLAYER_MAP, CELL_COUNT} from "./defines";
 
 export default class SceneService {
 	private _scene: HTMLCanvasElement;
 	private _ctx: CanvasRenderingContext2D;
 	private readonly _palyerColor: Map<number, string>;
+	private readonly _cellCount: number;
+
 	constructor(scene: HTMLCanvasElement) {
 		this._scene = scene;
 		this._ctx = scene.getContext('2d');
 		this._palyerColor = new Map();
+		this._cellCount = CELL_COUNT;
 		this.initPlayerColor();
 	}
 
@@ -18,7 +21,6 @@ export default class SceneService {
 		PLAYER_MAP.forEach((k, v) => {
 			this._palyerColor.set(v, k);
 		});
-		// console.log(this._palyerColor);
 	}
 
 	public clear(): void {
@@ -26,12 +28,12 @@ export default class SceneService {
 	}
 
 	public drawField(): void {
-		const cellSize = this.height / 8;
+		const cellSize = this.height / this._cellCount;
 
 		const cells = [];
-		for (let i = 0; i < 8; i++) {
+		for (let i = 0; i < this._cellCount; i++) {
 			let row = [];
-			for (let j = 0; j < 8; j++) {
+			for (let j = 0; j < this._cellCount; j++) {
 				row.push({
 					color: '#ffffff'
 				})
@@ -51,8 +53,13 @@ export default class SceneService {
 		const scaleFactor = this.width > this.height ? this.height : this.width;
 		const scale = scaleFactor / FIELD_SIZE;
 		
-		point.x *= scale;
-		point.y *= scale;
+		const x = point.x * scale;
+		const y = point.y * scale;
+
+		const radius = this.height / (this._cellCount * 2) * scale;
+		const color = this._palyerColor.get(player.id);
+
+		this._ctx.arc(x, y, radius, 0, 2 * Math.PI);
 	}
 
 	private drawCell(row: number, col: number, size: number, color: string): void {
