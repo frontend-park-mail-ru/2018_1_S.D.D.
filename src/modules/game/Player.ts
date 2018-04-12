21b82ff061c0da6d8ab66031631bf7e215071049
HustonMavr <huston.mavr@gmail.com>
2018-04-12 18:54:58 +0300

    added Direction and Point, player modified

--- src/modules/game/Player.ts
+++ src/modules/game/Player.ts
@@ -1,10 +1,15 @@
 'use strict';
 import InitialPlayerData from './InitialPlayerData';
+import Point from './Point';
+import {Direction} from './Direction';
 
 export default class Player {
     private _id: number;
     private _color: string;
     private _score: number;
+    private _direction: Direction;
+    private _changeDirection: Direction;
+    private _position: Point;
 
     constructor () {
         this._score = 0;
