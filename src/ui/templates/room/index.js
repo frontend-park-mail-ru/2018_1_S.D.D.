6e73598c961f099d069a67437df3a97bc00fb5e5
Dmitry Zaytsev <hello@dzaytsev.pro>
2018-06-02 06:39:23 +0300

    Lobby

--- src/ui/templates/room/index.js
+++ src/ui/templates/room/index.js
@@ -6,7 +6,7 @@ import './room.scss';
 import ServiceManager from '../../../modules/ServiceManager';
 
 export default {
-    addPlayersToRoom: function(tmpl, players, isowner) {
+    addPlayersToRoom: function(tmpl, players, isowner, lobbyId) {
         const place = tmpl.querySelector('.room__members');
         players.forEach(player => {
             const nm = addnewmmbr({
@@ -15,9 +15,38 @@ export default {
             });
             const nmdiv = document.createElement('div');
             nmdiv.classList.add('member');
+            nmdiv.classList.add(`member-${player.name}`);
             nmdiv.innerHTML = nm;
+            const rembtn = nmdiv.querySelector('.member__remove');
+            if (rembtn) {
+                rembtn.addEventListener('click', () => {
+                    const SM = new ServiceManager();
+                    console.log(player, {
+                        "class": "LobbyMessage",
+                        "action": "DISCONNECT",
+                        "id": lobbyId,
+                        "userId": player.id
+                    })
+                    SM.Net.send({
+                        "class": "LobbyMessage",
+                        "action": "DISCONNECT",
+                        "id": lobbyId,
+                        "userId": player.id
+                    });
+                });
+            }
             place.appendChild(nmdiv);
         });
+    },
+
+    removePlayersFromRoom: function(tmpl, player) {
+        const place = tmpl.querySelector('.room__members');
+        //console.log(place.querySelector(`.member-${player.name}`), `.member-${player}`)
+        const rem = place.querySelector(`.member-${player}`);
+        if (!rem) {
+            return;
+        }
+        rem.remove();
     },
 
     render: function(params) {
@@ -25,10 +54,12 @@ export default {
         elem.innerHTML = template(params);
 
         const startbtn = elem.querySelector('.start-btn');
-        startbtn.addEventListener('click', () => {
-            const SM = new ServiceManager();
-            SM.Router.re('/play');
-        });
+        if (startbtn) {
+            startbtn.addEventListener('click', () => {
+                const SM = new ServiceManager();
+                SM.Router.re('/play');
+            });
+        }
 
         return elem;
     }
